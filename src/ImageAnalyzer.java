079f19ab0df95c515d3ce76b081588491c5be6af
KikyoNanakusa <nanakusa.vrc@gmail.com>
2023-12-29 05:15:42 +0900

    Refactor ImageAnalyzer

--- src/ImageAnalyzer.java
+++ src/ImageAnalyzer.java
@@ -1,96 +1,69 @@
 import java.util.ArrayList;
 
 public class ImageAnalyzer {
-    private ArrayList<String> imagePaths;
     private ArrayList<UserImage> imageDataSet;
-    private int[] RvalueAverage;
-    private int[] GvalueAverage;
-    private int[] BvalueAverage;
-
+    private int[] RValueAverage;
+    private int[] GValueAverage;
+    private int[] BValueAverage;
+    private int[] RValueMedian;
+    private int[] GValueMedian;
+    private int[] BValueMedian;
+    private int[] LuminanceAverage;
+    private int[] LuminanceMedian;
 
     public ImageAnalyzer(final ArrayList<String> imagePaths) {
-        initializeData(imagePaths);
-
-        calclateAverageRvalue();
-        calclateAverageGvalue();
-        calclateAverageBvalue();
-        for (int i = 0; i < this.RvalueAverage.length; i++) {
-            System.out.println(i + " : " + this.RvalueAverage[i]);
+        this.imageDataSet = ImageDataInitializer.initializeData(imagePaths);
+        this.calculateStatistics();
+        for (int i = 0; i < 256; i++) {
+            System.err.println(i + ":" + RValueAverage[i]);
         }
     }
 
-    private void initializeData(ArrayList<String> imagePaths) {
-        this.imageDataSet = new ArrayList<UserImage>();
-        this.imagePaths = imagePaths;
-        for (String path : imagePaths) {
-            imageDataSet.add(new UserImage(path));
-        }
-    }
-
-    private void calclateAverageRvalue() {
-        int[] counts = new int[256];
-        
-        for (UserImage image : getImageDataSet()) {
-            int[] RvalueDistribution = image.getColorData().getRvalueDistribution();
-            for (int i = 0; i < counts.length; i++) {
-                counts[i] += RvalueDistribution[i];
-            }
-        }
-
-        for (int i = 0; i < counts.length; i++) {
-            counts[i] /= getImageDataSet().size();
-        }
-
-        this.RvalueAverage = counts;
-    }
-
-    private void calclateAverageGvalue() {
-        int[] counts = new int[256];
-        
-        for (UserImage image : getImageDataSet()) {
-            int[] GvalueDistribution = image.getColorData().getGvalueDistribution();
-            for (int i = 0; i < counts.length; i++) {
-                counts[i] += GvalueDistribution[i];
-            }
-        }
-
-        for (int i = 0; i < counts.length; i++) {
-            counts[i] /= getImageDataSet().size();
-        }
-
-        this.GvalueAverage = counts;
-    }
-
-    private void calclateAverageBvalue() {
-        int[] counts = new int[256];
-        
-        for (UserImage image : getImageDataSet()) {
-            int[] BvalueDistribution = image.getColorData().getBvalueDistribution();
-            for (int i = 0; i < counts.length; i++) {
-                counts[i] += BvalueDistribution[i];
-            }
-        }
-
-        for (int i = 0; i < counts.length; i++) {
-            counts[i] /= getImageDataSet().size();
-        }
-
-        this.BvalueAverage = counts;
+    private void calculateStatistics() {
+        ImageStatisticsCalculator calculator = new ImageStatisticsCalculator(imageDataSet);
+        this.RValueAverage = calculator.calculateAverageRValue();
+        this.GValueAverage = calculator.calculateAverageGValue();
+        this.BValueAverage = calculator.calculateAverageBValue();
+        this.RValueMedian = calculator.calculateMedianRValue();
+        this.GValueMedian = calculator.calculateMedianGValue();
+        this.BValueMedian = calculator.calculateMedianBValue();
+        this.LuminanceAverage = calculator.calculateAverageLuminance();
+        this.LuminanceMedian = calculator.calculateMedianLuminance();
     }
 
     private ArrayList<UserImage> getImageDataSet() {
         return this.imageDataSet;
     }
 
-    public int[] getRvalueAverage() {
-        return this.RvalueAverage;
+    public int[] getRValueAverage() {
+        return this.RValueAverage;
     }
 
-    public int[] getGvalueAverage() {
-        return this.GvalueAverage;
+    public int[] getGValueAverage() {
+        return this.GValueAverage;
     }
 
-    public int[] getBvalueAverage() {
-        return this.BvalueAverage;
+    public int[] getBValueAverage() {
+        return this.BValueAverage;
+    }
+
+    public int[] getRValueMedian() {
+        return this.RValueMedian;
+    }
+
+    public int[] getGValueMedian() {
+        return this.GValueMedian;
+    }
+
+    public int[] getBValueMedian() {
+        return this.BValueMedian;
+    }
+
+    public int[] getLuminanceAverage() {
+        return this.LuminanceAverage;
+    }
+
+    public int[] getLuminanceMedian() {
+        return this.LuminanceMedian;
     }
 }
---
+++ src/ImageStatisticsCalculator.java
@@ -0,0 +1,133 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ImageStatisticsCalculator {
+    private final int RGB_MAX = 256;
+    private ArrayList<UserImage> imageDataSet;
+
+    public ImageStatisticsCalculator(ArrayList<UserImage> imageDataSet) {
+        this.imageDataSet = imageDataSet;
+    }
+
+    public int[] calculateAverageRValue() {
+        return calculateAverageValue(ColorChannel.RED);
+    }
+
+    public int[] calculateAverageGValue() {
+        return calculateAverageValue(ColorChannel.GREEN);
+    }
+
+    public int[] calculateAverageBValue() {
+        return calculateAverageValue(ColorChannel.BLUE);
+    }
+
+    public int[] calculateMedianRValue() {
+        return calculateMedianValue(ColorChannel.RED);
+    }
+
+    public int[] calculateMedianGValue() {
+        return calculateMedianValue(ColorChannel.GREEN);
+    }
+
+    public int[] calculateMedianBValue() {
+        return calculateMedianValue(ColorChannel.BLUE);
+    }
+
+    public int[] calculateAverageLuminance() {
+        return calculateAverageValue(ColorChannel.LUMINANCE);
+    }
+
+    public int[] calculateMedianLuminance() {
+        return calculateMedianValue(ColorChannel.LUMINANCE);
+    }
+
+    private int[] calculateAverageValue(ColorChannel channel) {
+        int[] counts = new int[RGB_MAX];
+        for (UserImage image : imageDataSet) {
+            int[] valueDistribution = getColorDistribution(image, channel);
+            for (int i = 0; i < RGB_MAX; i++) {
+                counts[i] += valueDistribution[i];
+            }
+        }
+        for (int i = 0; i < RGB_MAX; i++) {
+            counts[i] /= imageDataSet.size();
+        }
+        return counts;
+    }
+
+    private int[] calculateMedianValue(ColorChannel channel) {
+        int imageDataSetSize = imageDataSet.size();
+        int[][] total = new int[RGB_MAX][imageDataSetSize];
+        int[] median = new int[RGB_MAX];
+
+        for (int i = 0; i < imageDataSetSize; i++) {
+            UserImage image = imageDataSet.get(i);
+            int[] valueDistribution = getColorDistribution(image, channel);
+            for (int j = 0; j < RGB_MAX; j++) {
+                total[j][i] = valueDistribution[j];
+            }
+        }
+
+        for (int i = 0; i < RGB_MAX; i++) {
+            Arrays.sort(total[i]);
+            median[i] = findMedian(total[i]);
+        }
+
+        return median;
+    }
+
+    private int[] calculateLuminanceDistribution() {
+        int[] luminanceDistribution = new int[RGB_MAX];
+        for (UserImage image : imageDataSet) {
+            int[][] R = image.getColorData().getR();
+            int[][] G = image.getColorData().getG();
+            int[][] B = image.getColorData().getB();
+            
+            for (int x = 0; x < R.length; x++) {
+                for (int y = 0; y < R[x].length; y++) {
+                    int luminance = calculateLuminance(R[x][y], G[x][y], B[x][y]);
+                    luminanceDistribution[luminance]++;
+                }
+            }
+        }
+        return luminanceDistribution;
+    }
+
+    private int calculateLuminance(int red, int green, int blue) {
+        return (int) (0.299 * red + 0.587 * green + 0.114 * blue);
+    }
+
+
+    private int[] getColorDistribution(UserImage image, ColorChannel channel) {
+        switch (channel) {
+            case RED:
+                return image.getColorData().getRvalueDistribution();
+            case GREEN:
+                return image.getColorData().getGvalueDistribution();
+            case BLUE:
+                return image.getColorData().getBvalueDistribution();
+            case LUMINANCE:
+                return calculateLuminanceDistribution();
+            default:
+                return new int[RGB_MAX]; // 安全のためのデフォルトケース
+        }
+    }
+
+    private int findMedian(int[] arr) {
+        int length = arr.length;
+        if (length % 2 == 0) {
+            int middle1 = arr[length / 2 - 1];
+            int middle2 = arr[length / 2];
+            return (middle1 + middle2) / 2;
+        } else {
+            return arr[length / 2];
+        }
+    }
+
+    private enum ColorChannel {
+        RED,
+        GREEN,
+        BLUE,
+        LUMINANCE,
+    }
+}

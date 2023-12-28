d889cfbf74b79d9eb80babdb20552b9bcbd729e6
KikyoNanakusa <nanakusa.vrc@gmail.com>
2023-12-28 23:04:54 +0900

    Delete a test statement

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        // ArgsChecker.Check(args);
         IImageScraper bookwalkerScraper = new BookWalkerScraper("https://bookwalker.jp/new/?qsto=st2");
-        // System.out.println(bookwalkerScraper.getImagePathes());        
     }
 
 }

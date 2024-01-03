fd96633237b63cab26b3afe3c4c9887b7e70da04
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-01-03 13:26:03 +0900

    Modify gradle dependences

---
+++ src/main/java/ArrayGraph.java
@@ -0,0 +1,48 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class ArrayGraph extends Application {
+   private double[] arrayData;
+
+   public ArrayGraph(double[] arrayData) {
+       this.arrayData = arrayData;
+   }
+
+   @Override
+   public void start(Stage stage) {
+       stage.setTitle("Array Graph");
+
+       // Defining the axes
+       final NumberAxis xAxis = new NumberAxis();
+       final NumberAxis yAxis = new NumberAxis();
+       xAxis.setLabel("Index");
+
+       // Creating the chart
+       final LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+
+       lineChart.setTitle("Array Data");
+
+       // Defining a series
+       XYChart.Series series = new XYChart.Series();
+       series.setName("Array Elements");
+
+       // Populating the series with data
+       for (int i = 0; i < arrayData.length; i++) {
+           series.getData().add(new XYChart.Data(i, arrayData[i]));
+       }
+
+       Scene scene = new Scene(lineChart, 800, 600);
+       lineChart.getData().add(series);
+
+       stage.setScene(scene);
+       stage.show();
+   }
+
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+}

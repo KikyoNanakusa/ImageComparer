b530b5bbe0e12dbc81809bd520b4d234dc6052d2
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-01-08 13:40:00 +0900

    Add UserImagePathFinder.java

---
+++ src/main/java/UserImagePathFinder.java
@@ -0,0 +1,7 @@
+public class UserImagePathFinder extends ImagePathFinder{
+   private final static String USER_IMAGE_PATH = "UserPreparedData/img";
+
+   public UserImagePathFinder() {
+       super(USER_IMAGE_PATH);
+   }
+}
